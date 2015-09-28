7c3863d18fc7333962756382e45e5760a6a57352
Dan Nguyen <dansonguyen@gmail.com>
2015-09-27 17:40:34 -0700

    disable animate on mobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,46 @@
+// disable mobile via: https://github.com/markdalgleish/stellar.js/issues/37
+var isMobile = {
+    Android: function() {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function() {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function() {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function() {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function() {
+        return navigator.userAgent.match(/IEMobile/i);
+    },
+    any: function() {
+        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+    }
+};
+
+
+
+
+
 $(document).ready(function(){
-// grab an element
-  var myElement = document.querySelector("#header-headroom");
-  // construct an instance of Headroom, passing the element
-  var headroom  = new Headroom(myElement);
-  // initialise
-  headroom.init();
+
   smoothScroll.init();
-  $.stellar({
-      horizontalScrolling: false
-  });
+  if( !isMobile.any() ){
+  // grab an element
+    var myElement = document.querySelector("#header-headroom");
+    // construct an instance of Headroom, passing the element
+    var headroom  = new Headroom(myElement);
+    // initialise
 
+    headroom.init();
+    $.stellar({
+        horizontalScrolling: false
+    });
+  }else{
+    $(".animate-intro").removeClass('animate-intro');
+    // disable animate
+  }
 })
 
-// hacky code to remove example-page constraints of p-wrapped image tags
-$(document).ready(function(){
-  $('.example-content p > img').wrap("<div class='imgwrap'></div>");
-})
