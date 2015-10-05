3267b9b1deed69722b3b19142ac4c26feb256d0e
Dan Nguyen <dansonguyen@gmail.com>
2015-10-05 11:28:56 -0700

    g search

--- _assets/javascripts/main.js
+++ _assets/javascripts/main.js
@@ -152,7 +152,8 @@ function debugMe(){
         color: 'hsl(330, 100%, 50%)',
         textShadow: '0 2px 0 hsl(330, 100%, 25%), 0 3px 2px hsla(330, 100%, 15%, 0.5), /* next */ 0 3px 0 hsl(350, 100%, 50%), 0 5px 0 hsl(350, 100%, 25%), 0 6px 2px hsla(350, 100%, 15%, 0.5), /* next */ 0 6px 0 hsl(20, 100%, 50%), 0 8px 0 hsl(20, 100%, 25%), 0 9px 2px hsla(20, 100%, 15%, 0.5), /* next */ 0 9px 0 hsl(50, 100%, 50%), 0 11px 0 hsl(50, 100%, 25%), 0 12px 2px hsla(50, 100%, 15%, 0.5), /* next */ 0 12px 0 hsl(70, 100%, 50%), 0 14px 0 hsl(70, 100%, 25%), 0 15px 2px hsla(70, 100%, 15%, 0.5), /* next */ 0 10px 0 hsl(90, 100%, 50%), 0 10px 0 hsl(90, 100%, 25%), 0 10px 2px hsla(90, 100%, 15%, 0.5)',
         fontFamily: "Menlo, Monaco, Consolas, 'Courier New', cursive",
-        backgroundColor: '#43FF43'
+        backgroundColor: '#43FF43',
+        textDecoration: 'blink'
     };
 
     var _smallfancystyle = {
